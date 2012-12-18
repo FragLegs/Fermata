97bab59fa0a9c1d8590cdbcc9b1edf1f9f5c6eb9
smiel <miel.shayne@gmail.com>
2012-12-18 14:26:37 -0500

    Added Help->About and some comments

--- Fermata/src/com/fermata/ui/FermataWindow.java
+++ Fermata/src/com/fermata/ui/FermataWindow.java
@@ -5,21 +5,18 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.BoxLayout;
-import javax.swing.JSplitPane;
+import javax.swing.JOptionPane;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import java.awt.Color;
-import javax.swing.UIManager;
-import javax.swing.border.LineBorder;
 import javax.swing.border.MatteBorder;
 import javax.swing.JLabel;
 import java.awt.SystemColor;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+@SuppressWarnings("serial")
 public class FermataWindow extends JFrame {
 
 	private JPanel contentPane;
@@ -52,13 +49,16 @@ public class FermataWindow extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
+		// add the menu bar
 		JMenuBar menuBar = new JMenuBar();
 		contentPane.add(menuBar, BorderLayout.NORTH);
 		
+		// File menu
 		JMenu mnFile = new JMenu("File");
 		mnFile.setMnemonic('F');
 		menuBar.add(mnFile);
 		
+		// File->Exit option
 		JMenuItem mntmExit = new JMenuItem("Exit");
 		mntmExit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -67,24 +67,35 @@ public class FermataWindow extends JFrame {
 		});
 		mnFile.add(mntmExit);
 		
+		// Help menu
 		JMenu mnHelp = new JMenu("Help");
 		mnHelp.setMnemonic('H');
 		menuBar.add(mnHelp);
 		
+		// Help->About option
 		JMenuItem mntmAbout = new JMenuItem("About");
+		mntmAbout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(contentPane, "Created by Matt Panning and Shayne Miel, 2012", "About", JOptionPane.INFORMATION_MESSAGE);
+			}
+		});
 		mnHelp.add(mntmAbout);
 		
+		// Build the panel where we will be displaying the staff
 		JPanel staffPanel = new JPanel();
 		contentPane.add(staffPanel, BorderLayout.CENTER);
 		
+		// temporary label
 		JLabel lblStaffHere = new JLabel("Staff Here");
 		staffPanel.add(lblStaffHere);
 		
+		// Build the panel where we will be displaying the keyboard
 		JPanel keyboardPanel = new JPanel();
 		keyboardPanel.setBorder(new MatteBorder(1, 0, 0, 0, (Color) new Color(0, 0, 0)));
 		keyboardPanel.setBackground(SystemColor.control);
 		contentPane.add(keyboardPanel, BorderLayout.SOUTH);
 		
+		// temporary label
 		JLabel lblKeyboardHere = new JLabel("Keyboard Here");
 		keyboardPanel.add(lblKeyboardHere);
 	}
