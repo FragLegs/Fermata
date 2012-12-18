da6e646ab234a9a8ccdd65a5dd26a7b8834e49c3
smiel <miel.shayne@gmail.com>
2012-12-18 13:43:39 -0500

    Initial UI

---
+++ Fermata/src/com/fermata/ui/FermataWindow.java
@@ -0,0 +1,84 @@
+package com.fermata.ui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.BoxLayout;
+import javax.swing.JSplitPane;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.Color;
+import javax.swing.UIManager;
+import javax.swing.border.LineBorder;
+import javax.swing.border.MatteBorder;
+import javax.swing.JLabel;
+import java.awt.SystemColor;
+
+public class FermataWindow extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FermataWindow frame = new FermataWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FermataWindow() {
+		setTitle("Fermata");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(null);
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JMenuBar menuBar = new JMenuBar();
+		contentPane.add(menuBar, BorderLayout.NORTH);
+		
+		JMenu mnFile = new JMenu("File");
+		mnFile.setMnemonic('F');
+		menuBar.add(mnFile);
+		
+		JMenuItem mntmExit = new JMenuItem("Exit");
+		mnFile.add(mntmExit);
+		
+		JMenu mnHelp = new JMenu("Help");
+		mnHelp.setMnemonic('H');
+		menuBar.add(mnHelp);
+		
+		JMenuItem mntmAbout = new JMenuItem("About");
+		mnHelp.add(mntmAbout);
+		
+		JPanel staffPanel = new JPanel();
+		contentPane.add(staffPanel, BorderLayout.CENTER);
+		
+		JLabel lblStaffHere = new JLabel("Staff Here");
+		staffPanel.add(lblStaffHere);
+		
+		JPanel keyboardPanel = new JPanel();
+		keyboardPanel.setBorder(new MatteBorder(1, 0, 0, 0, (Color) new Color(0, 0, 0)));
+		keyboardPanel.setBackground(SystemColor.control);
+		contentPane.add(keyboardPanel, BorderLayout.SOUTH);
+		
+		JLabel lblKeyboardHere = new JLabel("Keyboard Here");
+		keyboardPanel.add(lblKeyboardHere);
+	}
+}
