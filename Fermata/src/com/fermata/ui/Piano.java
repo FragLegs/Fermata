33dbab772c0f48911d5ca5e2d21bc689d820dd8b
FragLegs <miel.shayne@gmail.com>
2013-01-25 20:46:51 -0500

    Made Piano a JLayeredPane

--- Fermata/src/com/fermata/ui/Piano.java
+++ Fermata/src/com/fermata/ui/Piano.java
@@ -1,6 +1,7 @@
 package com.fermata.ui;
 
-import java.util.HashMap;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 
 import javax.swing.JLayeredPane;
 
@@ -8,19 +9,85 @@ import com.fermata.music.midi.MidiHelpers;
 
 @SuppressWarnings("serial")
 public class Piano extends JLayeredPane {
-	private HashMap<Integer, PianoKey> keyMap;
+	
+	// the keys
+	private PianoKey[] keys;
+	public PianoKey[] getKeys() { return keys; }
+	
+	// the number of white keys (tracked as convenience for resizing)
+	private int numWhite;
+	public int getNumWhite() { return numWhite; }
 	
 	public Piano()
 	{
-		keyMap = new HashMap<Integer, PianoKey>();
+		this(21, 108);
+	}
+	
+	public Piano(int lowKey, int highKey)
+	{
+		super();
 		
-		for (int i = 21; i <= 108; i++)
+		keys = new PianoKey[highKey - lowKey + 1];
+		
+		// for every key
+		for (int i = lowKey; i <= highKey; i++)
 		{
-			if (MidiHelpers.MidiToPianoKeyType(i) != PianoKeyType.BLACK)
-			{
-				
-			}
+			// create the key
+			PianoKey k = PianoKey.CreateKey(i);
+			
+			// store it
+			keys[i] = k;
+
+			// add it to the pane
+			// if it is a black key add it on the second layer
+			add(k, (k instanceof BlackKey) ? 1 : 0);
+			
+			// track number of white keys
+			if (k instanceof WhiteKey) numWhite++;
 		}
 		
+
+		
+		
+		
+	}
+	
+	public class PianoResizeListener implements ComponentListener
+	{
+
+		@Override
+		public void componentHidden(ComponentEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void componentMoved(ComponentEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void componentResized(ComponentEvent e) {
+			Piano p = (Piano)e.getComponent();
+			
+			// get the width of the white keys
+			int whiteWidth = getWidth() / p.getNumWhite();
+			int halfBlackWidth = whiteWidth / 3;
+			int offset = ((p.getNumWhite() * whiteWidth) - getWidth()) / 2; // center the keyboard
+			int whiteHeight = getHeight();
+			int blackHeight = (getHeight() / 3) * 2;
+			
+			for (PianoKey k : p.getKeys())
+			{
+			
+			}
+		}
+
+		@Override
+		public void componentShown(ComponentEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
 	}
 }
--- Fermata/src/com/fermata/ui/PianoKey.java
+++ Fermata/src/com/fermata/ui/PianoKey.java
@@ -16,10 +16,20 @@ public abstract class PianoKey extends JButton {
 	private int value;
 	public int getValue() { return value; }
 	
+	// the note name
+	private String name;
+	public String getName() { return name; }
+	
+	// the octave
+	private int octave;
+	public int getOctave() { return octave; }
+	
 	public PianoKey(int v)
 	{
 		value = v;
-		
+		name = KeyNames[v % 12];
+		octave = (v / 12) - 1;
+				
 		this.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e)
 			{
@@ -30,5 +40,23 @@ public abstract class PianoKey extends JButton {
 	}
 	
 	public abstract void updateBackground();
+	
+	// create a piano key from a numeric value
+	public static PianoKey CreateKey(int v)
+	{
+		switch (v % 12)
+		{
+		case 1:
+		case 3:
+		case 6:
+		case 8:
+		case 10:
+			return new BlackKey(v);
+		default:
+			return new WhiteKey(v);
+		}
+	}
+
+	public static String[] KeyNames = { "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B" }; 
 }
 
